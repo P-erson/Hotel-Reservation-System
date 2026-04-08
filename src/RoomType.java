6021e6cbf5c1d071800219d5e9ec5f89ec2097a4
MarkEhab2026 <markbahgat19@gmail.com>
2026-04-08 18:41:54 +0200

    Update RoomType.java

--- src/RoomType.java
+++ src/RoomType.java
@@ -1,3 +1,16 @@
-public class RoomType {
-    
+import java.util.ArrayList;
+
+public class RoomType
+{
+    private ArrayList<String> types;
+
+    public RoomType(ArrayList<String> types)
+    {
+        this.types = types;
+    }
+    public ArrayList<String> getTypes()
+    {
+        return this.types;
+    }
 }
+
