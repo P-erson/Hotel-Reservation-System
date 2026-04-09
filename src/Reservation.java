349300a8fcdfa4be7f04eaaab4e660a8bb155d82
MahmoudT493 <mahmoudkhaled262008@gmail.com>
2026-04-10 00:52:01 +0200

    Refactor Reservation class with additional methods

--- src/Reservation.java
+++ src/Reservation.java
@@ -1,8 +1,59 @@
-// Design a Reservation class with:
-//o Guest reference, room reference
-//o Check-in and check-out dates
-//o Reservation status (enum: PENDING, CONFIRMED, CANCELLED, COMPLET
-public class  Reservation
-        {
+import java.time.LocalDate;
+public class Reservation{
+    public enum ReservationStatus {PENDING, CONFIRMED, CANCELLED, COMPLETED};
+    private Guest guest;
+    private Room room;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private ReservationStatus status = ReservationStatus.PENDING;
+    public Reservation (Guest guest, Room room, String checkIn, String checkOut){
+        this.guest = guest;
+        this.room = room;
+        this.checkInDate = checkIn;
+        this.checkOutDate = checkOut;
+        this.status = ReservationStatus.CONFIRMED;
+    }
+    public void cancelReservation() {
+        this.status = ReservationStatus.CANCELLED;
+    }
+    public ReservationStatus getStatus() {
+        return status;
+    }
 
-        }
+    public void setStatus(ReservationStatus status) {
+        this.status = status;
+    }
+
+    public LocalDate getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(LocalDate checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public Guest getGuest() {
+        return guest;
+    }
+
+    public void setGuest(Guest guest) {
+        this.guest = guest;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public LocalDate getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(LocalDate checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+}
