64ed0eac87af49ad1fdb1467eb49d5edf4a540db
MarkEhab2026 <markbahgat19@gmail.com>
2026-04-11 01:03:45 +0200

    Update RoomType.java

--- src/RoomType.java
+++ src/RoomType.java
@@ -2,17 +2,53 @@ import java.util.ArrayList;
 
 public class RoomType
 {
-    private ArrayList<String> types = new ArrayList<String>();
+    private ArrayList<String> types;
+    private ArrayList<Double> prices;
 
-    public RoomType() {
-        types.add("SINGLE");
-        types.add("DOUBLE");
-        types.add("MASTER_SUITE");
-        types.add("ROYAL_SUITE");
+    public RoomType()
+    {
+        this.types = new ArrayList<>();
+        this.prices = new ArrayList<>();
+        this.types.add("Single");
+        this.types.add("Double");
+        this.types.add("Suite");
+        this.prices.add(100.00);
+        this.prices.add(200.00);
+        this.prices.add(300.00);
+
     }
-    public ArrayList<String> getTypes() { return types; }
+    public RoomType(ArrayList<String> types , ArrayList<Double> pries)
+    {
+        this.types = types;
+        this.prices = prices;
+    }
 
-    public void addType(String type) { types.add(type); }
-    public void removeType(String type) { types.remove(types.indexOf(type)); }
-    public void updateType (String oldType, String newtype) { types.set(types.indexOf(oldType), newtype); }
+    public ArrayList<String> getTypes()
+    {
+        return this.types;
+    }
+    public void setTypes(ArrayList<String> t)
+    {
+        this.types = t;
+    }
+
+    public ArrayList<Double> getPrices()
+    {
+        return this.prices;
+    }
+    public void setPrices(ArrayList<Double> p)
+    {
+        this.prices = p;
+    }
+
+    public void updatePrices(Double Oldprice , Double Newprice) // updates old price and replaces it with new price
+    {
+
+        int index = this.prices.indexOf(Oldprice);
+
+        if (index != -1)
+        {
+            this.prices.set(index, Newprice);
+        }
+    }
 }
