725d38ffb1ab07798e83f8454c3db234397dbb97
P-erson <mahmouds.fayed@outlook.com>
2026-04-12 08:57:41 +0200

    Fix indentation in Invoice.java

--- src/Invoice.java
+++ src/Invoice.java
@@ -3,23 +3,23 @@ import java.util.ArrayList;
 
 class Invoice{
     public enum PaymentMethod { CASH, CREDIT_CARD, ONLINE };
-private double totalAmount;
-private LocalDate paymentDate;
-private ArrayList<PaymentMethod> methods;
+    private double totalAmount;
+    private LocalDate paymentDate;
+    private ArrayList<PaymentMethod> methods;
 
-public void addPayment(PaymentMethod method) {
-    methods.add(method);
-}
+    public void addPayment(PaymentMethod method) {
+        methods.add(method);
+    }
 
-public void getSummary() {
-    System.out.println("Total Amount: " + totalAmount);
-    System.out.println("Payment Date: " + paymentDate);
-    System.out.println("Payment Methods: " + methods);
-}
-public Invoice(double totalAmount, LocalDate paymentDate) {
-    this.totalAmount = totalAmount;
-    this.paymentDate = paymentDate;
-    this.methods = new ArrayList<>();
-}
+    public void getSummary() {
+        System.out.println("Total Amount: " + totalAmount);
+        System.out.println("Payment Date: " + paymentDate);
+        System.out.println("Payment Methods: " + methods);
+    }
+    public Invoice(double totalAmount, LocalDate paymentDate) {
+        this.totalAmount = totalAmount;
+        this.paymentDate = paymentDate;
+        this.methods = new ArrayList<>();
+    }
 
 }
